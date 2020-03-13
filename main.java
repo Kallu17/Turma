f1f06e1a53048ea1b14473ce770e8a19cd364d63
Kallu17 <56933682+Kallu17@users.noreply.github.com>
2020-03-13 10:37:20 -0300

    Update main.java

--- main.java
+++ main.java
@@ -1,18 +1,19 @@
 import java.util.Scanner;
-import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.math.RoundingMode;
 
 public class Turma{
     public static void main(String[] args){
-        BigDecimal pctg = new BigDecimal("100");
+        DecimalFormat pctg = new DecimalFormat("#.00");
+        System.out.println("Homens: ");
+        int homens = new Scanner(System.in).nextInt();
         
-        System.out.println("Digite quantos homens tem na turma: ");
-        BigDecimal homens = new BigDecimal(new Scanner(System.in).nextInt());
-        System.out.println("Digite quantas mulheres tem na turma: ");
-        BigDecimal mulheres = new BigDecimal(new Scanner(System.in).nextInt());
+        System.out.println("Mulheres: ");
+        int mulheres = new Scanner(System.in).nextInt();
         
-        BigDecimal pctgHomens = ((homens).multiply(pctg)).divide(((homens).add(mulheres)), 2, BigDecimal.ROUND_HALF_EVEN);
-        BigDecimal pctgMulheres = ((mulheres).multiply(pctg)).divide(((homens).add(mulheres)), 2, BigDecimal.ROUND_HALF_EVEN);
+        double pctgHomens = (double)(homens*100)/(homens+mulheres);
+        double pctgMulheres = (double)(mulheres*100)/(mulheres+homens);
         
-        System.out.println("A turma é constituída por "+pctgHomens+"% de homens e "+pctgMulheres+"% de mulheres");
+        System.out.println("A turma é formada por "+pctg.format(pctgHomens)+"% de homens e por "+pctg.format(pctgMulheres)+"% de mulheres.");
     }
 }
